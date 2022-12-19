cd217fcab2cc63d87b01e9bc68c71c9b38f94131
Prasanth Remella <98454591+prasanth-surya@users.noreply.github.com>
2022-12-19 12:50:26 +0530

    light & dark mode

---
+++ app.js
@@ -0,0 +1,53 @@
+const toggleSwitch = document.querySelector("#check1");
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+function imageMode(color) {
+  image1.src = `/undraw_business_chat_re_gg4h-${color}.svg`;
+  image2.src = `/undraw_writer_q06d-${color}.svg`;
+  image3.src = `/undraw_engineering_team_a7n2-${color}.svg`;
+}
+
+function darkMode() {
+  nav.style.backgroundColor = "rgba(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageMode("dark");
+}
+
+function lightMode() {
+  nav.style.backgroundColor = "rgba(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMode("light");
+}
+
+function switchmode(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    lightMode();
+  }
+}
+
+toggleSwitch.addEventListener("change", switchmode);
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
